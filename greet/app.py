d7021c5135ec2b7456384eef8a498e373a096b26
Gavin Feriancek <gferiancek@gmail.com>
2023-12-24 04:03:34 -0500

    Added simple greet app to pass test spec

---
+++ greet/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/welcome")
+def welcome():
+    """Welcomes user"""
+
+    return "welcome"
+
+
+@app.route("/welcome/home")
+def welcome_home():
+    """Welcomes user home"""
+
+    return "welcome home"
+
+
+@app.route("/welcome/back")
+def welcome_back():
+    """Welcomes user upon returning back from their outing"""
+
+    return "welcome back"
